6a482260bd5efa4b766b75849b7b4e0f9e515964
Christian Hansen <447922@rts-365.dk>
2017-12-08 10:25:45 +0100

    Hjemmesiden vises med port 80 og virker 100%

--- app/app.js
+++ app/app.js
@@ -4,15 +4,15 @@ var app = express();
 var dataFile = require('./data/data.json');
 var io = require('socket.io')();
 
-app.set('port', process.env.PORT || 3000 );
+app.set('port', process.env.PORT || 80 );
 app.set('appData', dataFile);
 app.set('view engine', 'ejs');
-app.set('views', 'app/views');
+app.set('views', 'views');
 
 app.locals.siteTitle = 'Roux Meetups';
 app.locals.allSpeakers = dataFile.speakers;
 
-app.use(express.static('app/public'));
+app.use(express.static('public'));
 app.use(require('./routes/index'));
 app.use(require('./routes/speakers'));
 app.use(require('./routes/feedback'));
